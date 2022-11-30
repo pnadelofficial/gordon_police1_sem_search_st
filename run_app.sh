f21b7e4d4c092c48158b29d165610464cb178802
peter nadel <peter.m.nadel@gmail.com>
2022-11-30 14:20:04 -0500

    bash update

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-git clone https://github.com/pnadelofficial/gordon_police1_sem_search_st.git
-cd gordon_police1_sem_search_st
-pip install -r requirements.txt
-streamlit run st_app.py
+if [ -d '/gordon_police1_sem_search_st']; then
+    echo "Running app..."
+    streamlit run st_app.py
+else
+    git clone https://github.com/pnadelofficial/gordon_police1_sem_search_st.git
+    cd gordon_police1_sem_search_st
+    pip install -r requirements.txt
+    echo 'Running app...'
+    streamlit run st_app.py
+fi
